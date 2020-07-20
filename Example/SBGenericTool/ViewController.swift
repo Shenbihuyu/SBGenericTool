b8a4105b17e84334831ff6a220d1a762b2d73aea
waing <waingjianpeng@outlook.com>
2020-07-20 10:12:55 +0800

    Initial commit

---
+++ Example/SBGenericTool/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SBGenericTool
+//
+//  Created by waing on 07/20/2020.
+//  Copyright (c) 2020 waing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
