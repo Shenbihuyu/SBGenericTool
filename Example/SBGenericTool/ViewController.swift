52e3dbb05751b224fb33bc3ac81f62f975ba3a98
waing <waingjianpeng@outlook.com>
2020-07-28 13:38:53 +0800

    优化Demo 排版

--- Example/SBGenericTool/ViewController.swift
+++ Example/SBGenericTool/ViewController.swift
@@ -15,10 +15,11 @@ enum CellType : Int, IndexPathGeneticable, CaseIterable  {
     case shareApp = 0
     case recommendApp
     case comments
-    case suggest
+    
+    case suggest = 100
     case privacy
     
-    case dev = 100
+    case dev = 200
 }
 
 class ViewController: UITableViewController {
@@ -57,13 +58,12 @@ class ViewController: UITableViewController {
 
 struct UserInfoCopy: DevOptionCellAble, DevOptionCopyable {
     var copyString: String {
-        return "广告ID"
+        return "广告ID xxxxxxxx"
     }
     var detail: String? { return nil }
     var title: String {
         return "广告ID"
     }
-    
 }
 
 struct VipDownloadCount: DevOptionCountable, DevOptionCellAble {
@@ -71,10 +71,7 @@ struct VipDownloadCount: DevOptionCountable, DevOptionCellAble {
     var title: String {
         return "vip下载次数"
     }
-    func addCount() {
-        setCount(self.count + 1)
-    }
 }
 
 let 问卷星意见link = "https://www.wjx.cn/jq/84580778.aspx"
-let 隐私政策link = "https://www.shenbihuyu.com/app_privacy.html"
+let 隐私政策link   = "https://www.shenbihuyu.com/app_privacy.html"
